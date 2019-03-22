1433c258043502a42203f8dbcc61c209bb5eb119
Dmytro Mytysh <mitishdima@yandex.ua>
2019-03-22 20:50:20 +0100

    Creating post/just a fish

---
+++ src/Components/Posts/ButtonsForPostCreating.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Tooltip from '@material-ui/core/Tooltip';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+
+const styles = theme => ({
+  root: {
+    width: 500,
+  },
+  fab: {
+    margin: theme.spacing.unit * 2,
+  },
+  absolute: {
+    position: 'absolute',
+    bottom: theme.spacing.unit * 2,
+    right: theme.spacing.unit * 3,
+  },
+});
+
+function PositionedTooltips(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      {/* <Grid container justify="center">
+        <Grid item>
+          <Tooltip title="Add" placement="top-start">
+            <Button>top-start</Button>
+          </Tooltip>
+          <Tooltip title="Add" placement="top">
+            <Button>top</Button>
+          </Tooltip>
+          <Tooltip title="Add" placement="top-end">
+            <Button>top-end</Button>
+          </Tooltip>
+        </Grid>
+      </Grid>
+      <Grid container justify="center">
+        <Grid item xs={6}>
+          <Tooltip title="Add" placement="left-start">
+            <Button>left-start</Button>
+          </Tooltip>
+          <br />
+          <Tooltip title="Add" placement="left">
+            <Button>left</Button>
+          </Tooltip>
+          <br />
+          <Tooltip title="Add" placement="left-end">
+            <Button>left-end</Button>
+          </Tooltip>
+        </Grid>
+        <Grid item container xs={6} alignItems="flex-end" direction="column" spacing={0}>
+          <Grid item>
+            <Tooltip title="Add" placement="right-start">
+              <Button>right-start</Button>
+            </Tooltip>
+          </Grid>
+          <Grid item>
+            <Tooltip title="Add" placement="right">
+              <Button>right</Button>
+            </Tooltip>
+          </Grid>
+          <Grid item>
+            <Tooltip title="Add" placement="right-end">
+              <Button>right-end</Button>
+            </Tooltip>
+          </Grid>
+        </Grid>
+      </Grid> */}
+      <Grid container justify="center">
+        <Grid item>
+          <Tooltip title="Save" placement="bottom-start">
+            <Button>Save</Button>
+          </Tooltip>
+          
+         
+          <Tooltip title="Cancel" placement="bottom-end">
+        <IconButton aria-label="Delete">
+          <DeleteIcon />
+        </IconButton>
+      </Tooltip>
+   
+        </Grid>
+      </Grid>
+      </div>
+    
+  );
+}
+
+PositionedTooltips.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(PositionedTooltips);
