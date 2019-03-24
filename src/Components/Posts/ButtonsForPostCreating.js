a3c0ee0fc88db286fa398215a856f80e17542d9b
Dmytro Mytysh <mitishdima@yandex.ua>
2019-03-24 13:37:31 +0100

    Adding buttons to post

--- src/Components/Posts/ButtonsForPostCreating.js
+++ src/Components/Posts/ButtonsForPostCreating.js
@@ -1,97 +1,88 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import Tooltip from '@material-ui/core/Tooltip';
-import DeleteIcon from '@material-ui/icons/Delete';
-import IconButton from '@material-ui/core/IconButton';
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from 'classnames';
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Tooltip from "@material-ui/core/Tooltip";
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
+import PhotoCamera from "@material-ui/icons/PhotoCamera";
+import SaveIcon from '@material-ui/icons/Save';
 
 const styles = theme => ({
   root: {
-    width: 500,
+    width: 500
   },
   fab: {
-    margin: theme.spacing.unit * 2,
+    margin: theme.spacing.unit * 2
   },
   absolute: {
-    position: 'absolute',
+    position: "absolute",
     bottom: theme.spacing.unit * 2,
-    right: theme.spacing.unit * 3,
+    right: theme.spacing.unit * 3
   },
+  button: {
+    margin: theme.spacing.unit
+  },
+  leftIcon: {
+    marginRight: theme.spacing.unit,
+  },
+  rightIcon: {
+    marginLeft: theme.spacing.unit,
+  },
+  iconSmall: {
+    fontSize: 20,
+  },
+  input: {
+    display: "none"
+  }
 });
 
 function PositionedTooltips(props) {
   const { classes } = props;
   return (
     <div className={classes.root}>
-      {/* <Grid container justify="center">
-        <Grid item>
-          <Tooltip title="Add" placement="top-start">
-            <Button>top-start</Button>
-          </Tooltip>
-          <Tooltip title="Add" placement="top">
-            <Button>top</Button>
-          </Tooltip>
-          <Tooltip title="Add" placement="top-end">
-            <Button>top-end</Button>
-          </Tooltip>
-        </Grid>
-      </Grid>
-      <Grid container justify="center">
-        <Grid item xs={6}>
-          <Tooltip title="Add" placement="left-start">
-            <Button>left-start</Button>
-          </Tooltip>
-          <br />
-          <Tooltip title="Add" placement="left">
-            <Button>left</Button>
-          </Tooltip>
-          <br />
-          <Tooltip title="Add" placement="left-end">
-            <Button>left-end</Button>
-          </Tooltip>
-        </Grid>
-        <Grid item container xs={6} alignItems="flex-end" direction="column" spacing={0}>
-          <Grid item>
-            <Tooltip title="Add" placement="right-start">
-              <Button>right-start</Button>
-            </Tooltip>
-          </Grid>
-          <Grid item>
-            <Tooltip title="Add" placement="right">
-              <Button>right</Button>
-            </Tooltip>
-          </Grid>
-          <Grid item>
-            <Tooltip title="Add" placement="right-end">
-              <Button>right-end</Button>
-            </Tooltip>
-          </Grid>
-        </Grid>
-      </Grid> */}
       <Grid container justify="center">
         <Grid item>
-          <Tooltip title="Save" placement="bottom-start">
-            <Button>Save</Button>
-          </Tooltip>
-          
-         
+        <Tooltip title="SaveME!" placement="bottom-end">
+            <Button variant="contained" size="small" className={classes.button}>
+              Save
+            <SaveIcon className={classNames(classes.leftIcon, classes.iconSmall)} />
+            </Button>
+            </Tooltip>
           <Tooltip title="Cancel" placement="bottom-end">
-        <IconButton aria-label="Delete">
-          <DeleteIcon />
-        </IconButton>
-      </Tooltip>
-   
+            <IconButton aria-label="Delete">
+              <DeleteIcon />
+            </IconButton>
+            
+          </Tooltip>
+          <Tooltip title="Input Photo" placement="bottom-end">
+          {/* <input
+              accept="image/*"
+              className={classes.input}
+              id="icon-button-file"
+              type="file"
+            />  */}
+            {/* <label htmlFor="icon-button-file"> */}
+              <IconButton
+                color="primary"
+                className={classes.button}
+                component="span"
+              >
+                <PhotoCamera />
+              </IconButton>
+            {/* </label> */}
+            </Tooltip>
+    
         </Grid>
       </Grid>
-      </div>
-    
+    </div>
   );
 }
 
 PositionedTooltips.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(PositionedTooltips);
